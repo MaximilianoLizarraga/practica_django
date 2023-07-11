12c11abaa8b0374db13f0e43845226d6c53f0481
unknown <maxi.lizarraga.91@gmail.com>
2023-07-10 22:30:13 -0300

    se creo la segunda vista

--- sistema_coder/urls.py
+++ sistema_coder/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from sistema_coder.views import saludar
+from sistema_coder.views import saludar_con_fecha
 
 urlpatterns = [
 path("admin/", admin.site.urls),
 path("saludo/", saludar),
+path("saludo-hoy",saludar_con_fecha)
 ]
--- sistema_coder/views.py
+++ sistema_coder/views.py
@@ -8,3 +8,10 @@ def saludar(request):
     saludo = "Hola querido usuario"
     pagina_html = HttpResponse(saludo)
     return pagina_html
+
+
+def saludar_con_fecha(request):
+    hoy = datetime.now()
+    saludo = f"Hola querido usuario, fecha:{hoy.day}/{hoy.month}"
+    respuesta_http = HttpResponse(saludo)
+    return respuesta_http
